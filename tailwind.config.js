2bcd76b750d4ad542cb8330e0beaccd4ab8baa39
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-10-20 23:00:38 +0700

    chore: :wrench: update some configuration files for tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
