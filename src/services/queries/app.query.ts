03596dd04e364a93722ae41d01345af1e1275fcc
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-10-21 20:46:53 +0700

    feat: :triangular_flag_on_post: add app services for connect to the freetogame API

--- src/services/queries/app.query.ts
+++ src/services/queries/app.query.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import type { App, GetAppsProps } from '@/types/app';
-import { getApps } from '../api/car.service';
+import { getApps } from '../api/app.service';
 
 export const useAppsQuery = (params: GetAppsProps) =>
   useQuery<App[]>(['getApps', { params }], async () => {
