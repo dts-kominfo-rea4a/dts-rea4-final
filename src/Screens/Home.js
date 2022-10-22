203f52e392ffdd0597bad0cfa33a832ef8ea6ca5
Dicky Pratama <dickypratama4792@gmail.com>
2022-10-22 19:31:07 +0700

    update home screen

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -3,21 +3,35 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import TopNews from "../Components/TopNews";
 import News from "../Components/News";
-import NewsMenu from '../Components/NewsMenu';
+import SortSelect from "../Components/SortSelect";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getNews} from "../Features/newsSlice";
+import {Backdrop, CircularProgress} from "@mui/material";
+import NewsMenu from "../Components/NewsMenu";
 
+const Loading = () => {
+    return (
+        <Backdrop
+            sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}}
+            open={true}
+        >
+            <CircularProgress color="inherit"/>
+        </Backdrop>
+    )
+}
+
+export default function Home({category}) {
+    const {loading, news} = useSelector((state) => ({...state.app}))
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getNews({category}))
+    }, [category]);
 
     return (
-<<<<<<< HEAD
-      <React.Fragment>
-        <NewsMenu />
-        <CssBaseline />
-        <Container maxWidth={'xl'}>
-          <TopNews category={category} />
-          <News category={category} />
-        </Container>
-      </React.Fragment>
-=======
         <React.Fragment>
+            <NewsMenu />
             <CssBaseline/>
             {
                 loading ? (<Loading/>) : (
@@ -29,6 +43,5 @@ import NewsMenu from '../Components/NewsMenu';
                 )
             }
         </React.Fragment>
->>>>>>> dicky-dev
     );
 }
