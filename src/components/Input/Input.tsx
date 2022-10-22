95f3e447293d50aef4cbd81350805b12a46720c9
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-10-22 14:11:06 +0700

    feat: :art: improvement types, props, and dark mode to the Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react';
+import { FormEvent, HTMLInputTypeAttribute, InputHTMLAttributes } from 'react';
 import type {
   FieldErrors,
   FieldValues,
@@ -10,14 +10,18 @@ export interface Props<
   T extends FieldValues,
   U extends FieldValues = FieldValues
 > extends InputHTMLAttributes<HTMLInputElement> {
+  type?: HTMLInputTypeAttribute;
   name: string;
   label?: string;
   disabled?: boolean;
-  register: UseFormRegister<T>;
+  register?: UseFormRegister<T>;
   errors?: FieldErrors<U>;
+  onChange?: (e: FormEvent<HTMLInputElement>) => void;
+  className?: string;
 }
 
 const Input = <T extends FieldValues, U extends FieldValues>({
+  type,
   disabled = false,
   placeholder,
   errors,
@@ -25,24 +29,36 @@ const Input = <T extends FieldValues, U extends FieldValues>({
   name,
   value,
   register,
+  onChange,
+  className,
   ...rest
 }: Props<T, U>) => {
   return (
     <div className="flex flex-col">
-      <label className="flex" htmlFor={name}>
-        {label ?? ''}
-      </label>
+      {label && (
+        <label
+          className="flex font-medium text-gray-900 dark:text-gray-300"
+          htmlFor={name}
+        >
+          {label}
+        </label>
+      )}
       <input
-        className={`w-full p-2 mb-2 border focus:ring focus:ring-blue-500 focus:border-blue-500 rounded-lg ${
-          errors && errors[name as keyof U]
-            ? 'border-red-500 focus:ring-red-500 focus:border-red-500'
-            : ''
-        }`}
+        type={type}
+        className={
+          className ||
+          `block bg-gray-50 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white mb-2 border dark:border-2 border-gray-300 text-gray-900 focus:ring focus:ring-blue-500 focus:border-blue-500 rounded-lg ${
+            errors && errors[name as keyof U]
+              ? 'border-red-500 dark:border-red-500 dark:border-2 focus:ring-red-500 focus:border-red-500'
+              : ''
+          }`
+        }
         placeholder={placeholder}
         disabled={disabled}
         value={value}
-        {...register(name as Path<T>)}
+        {...(register ? register(name as Path<T>) : {})}
         {...rest}
+        onChange={onChange}
       />
       {errors && errors[name as keyof U] && (
         <span className="mb-2 text-xs text-red-500">
