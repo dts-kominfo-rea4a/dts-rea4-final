7e7e4261c23989dc6c964047ee5367c509dfb6a1
fatwakb <46946605+fatwakb@users.noreply.github.com>
2022-10-23 10:22:47 +0700

    feat: menambahkan app drawer

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,8 +6,54 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
-const AppNavbar = () => {
+const drawerWidth = 240;
+const navItems = ["Sign In", "Sign Out"];
+
+const AppNavbar = (props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{ textAlign: "center" }}
+    >
+      <Typography
+        variant="h6"
+        sx={{ my: 2 }}
+      >
+        News Portal
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem
+            key={item}
+            disablePadding
+          >
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
     <Box sx={{ flexGrow: 1, padding: 1 }}>
       <AppBar
@@ -50,12 +96,33 @@ const AppNavbar = () => {
             edge="start"
             color="inherit"
             aria-label="menu"
+            onClick={handleDrawerToggle}
             sx={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <Box component="nav">
+        <Drawer
+          anchor="right"
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
     </Box>
   );
 };
