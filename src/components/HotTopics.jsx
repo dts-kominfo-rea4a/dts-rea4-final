e2a4e2f40888959459b24aab2f3f9e1f0b5b319b
fatwakb <46946605+fatwakb@users.noreply.github.com>
2022-10-23 12:52:48 +0700

    feat: menyesuaikan hot topic dan menambahkan dayjs

--- src/components/HotTopics.jsx
+++ src/components/HotTopics.jsx
@@ -6,129 +6,123 @@ import CardMedia from "@mui/material/CardMedia";
 import CardActions from "@mui/material/CardActions";
 import Grid from "@mui/material/Grid";
 import { useGetTopHeadlinesQuery } from "../services/newsApi";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 const HotTopics = () => {
   const { data, error, isLoading } = useGetTopHeadlinesQuery();
+  dayjs.extend(relativeTime);
 
   return (
-    <>
-      <div className="App">
-        {error ? (
-          <>Oh no, there was an error</>
-        ) : isLoading ? (
-          <>Loading...</>
-        ) : data ? (
-          <>
-            <h3>{data.articles[0].title}</h3>
-          </>
-        ) : null}
-      </div>
-      <Box>
-        <Typography
-          variant="h3"
-          fontWeight={600}
-          gutterBottom
-        >
-          Hot Topics
-        </Typography>
-        <Card sx={{ boxShadow: 0 }}>
-          <Grid
-            container
-            spacing={4}
+    <Box>
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <Typography
+            variant="h3"
+            fontWeight={600}
+            gutterBottom
           >
+            Hot Topics
+          </Typography>
+          <Card sx={{ boxShadow: 0 }}>
             <Grid
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={8}
-              xl={8}
+              container
+              spacing={4}
             >
-              <Box
-                sx={{
-                  position: "relative",
-                }}
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={12}
+                lg={8}
+                xl={8}
               >
-                <CardMedia
-                  component="img"
-                  image="https://placekitten.com/1000/1000"
-                  alt="green iguana"
-                  sx={{
-                    height: { xs: 250, sm: 400, md: 400, lg: 400, xl: 400 },
-                  }}
-                />
                 <Box
                   sx={{
-                    padding: 2,
-                    position: "absolute",
-                    top: "50%",
-                    color: "white",
-                    maxWidth: 500,
+                    position: "relative",
                   }}
                 >
-                  <Typography
-                    variant="h4"
-                    gutterBottom
+                  <CardMedia
+                    component="img"
+                    image={data.articles[0].urlToImage}
+                    alt="green iguana"
                     sx={{
-                      fontWeight: 600,
-                      fontSize: { xs: 16, sm: 32, md: 32, lg: 32, xl: 32 },
+                      height: { xs: 250, sm: 400, md: 400, lg: 400, xl: 400 },
+                    }}
+                  />
+                  <Box
+                    sx={{
+                      padding: 2,
+                      position: "absolute",
+                      top: "50%",
+                      color: "white",
+                      maxWidth: 500,
                     }}
                   >
-                    Massa tortor nibh nulla condimentum imperdiet scelerisque...
-                  </Typography>
-                  <CardActions>
                     <Typography
+                      variant="h4"
                       gutterBottom
-                      variant="body2"
-                      component="div"
-                      sx={{ fontWeight: 700 }}
+                      sx={{
+                        fontWeight: 600,
+                        fontSize: { xs: 16, sm: 32, md: 32, lg: 32, xl: 32 },
+                      }}
                     >
-                      2 Hour Ago
+                      {data.articles[0].title.substring(0, 55)}...read more
                     </Typography>
-                    <Typography
-                      gutterBottom
-                      variant="body2"
-                      component="div"
-                      sx={{ fontWeight: 700 }}
-                    >
-                      CNN Indonesia
-                    </Typography>
-                  </CardActions>
+                    <CardActions>
+                      <Typography
+                        gutterBottom
+                        variant="body2"
+                        component="div"
+                        sx={{ fontWeight: 700 }}
+                      >
+                        {dayjs(data.articles[0].publishedAt).fromNow()}
+                      </Typography>
+                      <Typography
+                        gutterBottom
+                        variant="body2"
+                        component="div"
+                        sx={{ fontWeight: 700 }}
+                      >
+                        {data.articles[0].source.name}
+                      </Typography>
+                    </CardActions>
+                  </Box>
                 </Box>
-              </Box>
-            </Grid>
-            <Grid
-              item
-              xs={0}
-              sm={0}
-              md={0}
-              lg={4}
-              xl={4}
-              sx={{
-                display: {
-                  xs: "none",
-                  sm: "none",
-                  md: "none",
-                  lg: "block",
-                  xl: "block",
-                },
-              }}
-            >
-              <Typography
-                variant="body1"
-                sx={{ fontSize: 21 }}
+              </Grid>
+              <Grid
+                item
+                xs={0}
+                sm={0}
+                md={0}
+                lg={4}
+                xl={4}
+                sx={{
+                  display: {
+                    xs: "none",
+                    sm: "none",
+                    md: "none",
+                    lg: "block",
+                    xl: "block",
+                  },
+                }}
               >
-                Nisi, sagittis aliquet sit rutrum. Nunc, id vestibulum quam
-                ornare adipiscing. Pellentesque sed turpis nunc gravida
-                pharetra, sit nec vivamus pharetra. Velit, dui, egestas nisi,
-                elementum mattis mauris, magnis. Massa tortor nibh nulla
-                condimentum imperdiet scelerisque... read more
-              </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{ fontSize: 21 }}
+                >
+                  {data.articles[0].content.substring(0, 160)}...read more
+                </Typography>
+              </Grid>
             </Grid>
-          </Grid>
-        </Card>
-      </Box>
-    </>
+          </Card>
+        </>
+      ) : null}
+    </Box>
   );
 };
 
