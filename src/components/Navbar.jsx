12538212a97973b9cf35f61fee2770895740a249
Achmad Ibrahim Humam <55597186+humamisme@users.noreply.github.com>
2022-10-25 08:56:16 +0700

    fix tailwind setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/logo.svg";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="bg-red-500">
+      <a href="/">
+        <img src={logo} alt="logo" className="h-11"/>
+      </a>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import NotFoundPage from './pages/NotFoundPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/home' element={<HomePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-      <Route path='/not-found' element={<NotFoundPage />} />
-      
-      <Route path='*' element={<Navigate to={"/not-found"} />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/not-found" element={<NotFoundPage />} />
+
+        <Route path="*" element={<Navigate to={"/not-found"} />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
