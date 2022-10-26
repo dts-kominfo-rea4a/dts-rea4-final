4fc4cd7a9beeb17fe23794b03788e85db8b7f1fd
Fiandika <kiatama54@gmail.com>
2022-10-26 20:30:25 +0700

    bismillah-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 const Navbar = ({ isLoggedIn, logout }) => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+    
+      
       <Link to="/">
         <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
           TriFlix.
