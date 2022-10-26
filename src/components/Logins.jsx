7954c19650504fafbb97559a4e24512d8ac591da
Fiandika <kiatama54@gmail.com>
2022-10-26 20:36:20 +0700

    bismillah-3

--- src/components/Logins.jsx
+++ src/components/Logins.jsx
@@ -80,7 +80,7 @@ useEffect(
       <div className="w-full h-screen">
         <img
           className="hidden sm:block absolute w-full h-full object-cover"
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          src="https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg"
           alt="/"
         />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
