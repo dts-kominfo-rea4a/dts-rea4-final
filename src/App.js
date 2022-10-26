9bda690b288f56e8d4db6cfae52cf9dca144a5cf
Ferdi Hasan <ferdihasanpwd@gmail.com>
2022-10-26 14:59:43 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import HomePage from "./containers/HomePage";
 import MovieDetail from "./containers/MovieDetailPage";
+import Footer from "./components/Footer";
 function App() {
   return (<>
     <div className="App">
       <NavBar />
       <HomePage />
+      <Footer />
     </div>
   </>
   );
